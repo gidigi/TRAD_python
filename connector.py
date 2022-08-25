500b1ca34fbf5a55844a6218052da2fc5f0c2f25
Soldy <4786022+Soldy@users.noreply.github.com>
2022-08-25 20:46:43 +0100

    login and others
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- connector.py
+++ connector.py
@@ -1,28 +1,29 @@
-import http.client
-import json
+import http.client,json
 
 
 class TRAD:
     def __headers(self):
         return ({
-             'Content-type' : 'application/json',
+             'Content-type' : 'application/json;charset=utf-8',
              'Hash' : self.__hash,
-             'Auth' : self.__hash,
+             'Auth' : self.__auth,
              'Client' : self.__client,
              'User' : self.__user,
              'Type' : self.__type,
              'Company' : self.__company
         })
-    def __get(self, url: str)->dict:
+    def __get(self, url: str)->tuple:
         self.__conn.request('GET', url, '', self.__headers()) 
         response = self.__conn.getresponse()
         self.__last_response = response
-        return response.read().decode()
-    def __post(self, url: str, post:dict)->dict:
+        if response.status == 200:
+            return response.read().decode()
+    def __post(self, url: str, post:dict)->tuple:
         self.__conn.request('POST', url, json.dumps(post), self.__headers()) 
         response = self.__conn.getresponse()
         self.__last_response = response
-        return response.read().decode()
+        if response.status == 200:
+            return response.read().decode()
     def __put(self, url: str, post:dict)->tuple:
         self.__conn.request('PUT', url, json.dumps(post), self.__headers()) 
         response = self.__conn.getresponse()
@@ -41,34 +42,66 @@ class TRAD:
         self.__last_response = response
         if response.status == 200:
             return response.read().decode()
-    def hello(self)->str:
+    def hello(self)->bool:
         result = json.loads(self.__get('/hello'))
         try:
             self.__client = result['client']
             self.__last_failed = False
-            return self.__client
+            return True
         except (NameError, AttributeError):
             self.__last_failed = True
-        return ''
+        return False
     def login(self, login:str, password:str)->bool:
-        result = json.loads(self.__post('/login', {'login': login,'password':password}))
+        result = json.loads(
+            self.__post(
+                '/login', 
+                {'login': login,'password':password}
+            )
+        )
         try:
-            self.__auth = result['data']['auth']
+            self.__hash = result['data']['auth']
             self.__user = result['data']['id']
             self.__last_failed = False
-            return self.__user
+            return True
         except (NameError, AttributeError):
             self.__last_failed = True
-        return ''
+        return False
+    def register(
+      self,
+      name:str,
+      email:str,
+      phone:str,
+      password:str
+    )->int:
+        result = json.loads(
+            self.__post(
+                '/user', 
+                {
+                    'name'     : name,
+                    'email'    : email,
+                    'phone'    : phone,
+                    'password' : password
+                }
+            )
+        )
     def tasks(self)->dict:
         result = json.loads(self.__get('/tasks'))
         return result['data']
+    def taskAdd(self, name:str)->dict:
+        result = json.loads(self.__post('/task', {'name':name}))
+        return result
     def activities(self)->dict:
         result = json.loads(self.__get('/activities'))
         return result['data']
+    def activityAdd(self, name:str)->dict:
+        result = json.loads(self.__post('/activity', {'name':name}))
+        return result
     def projects(self)->dict:
-        result = json.loads(self.__get('/activities'))
+        result = json.loads(self.__get('/projects'))
         return result['data']
+    def projectAdd(self, name:str)->dict:
+        result = json.loads(self.__post('/project', {'name':name}))
+        return result
     def report_start(
       self,
       project:str,
@@ -143,17 +176,17 @@ class TRAD:
         result = json.loads(self.__post(
           '/standup',
           {
-              'projects':projects,
-              'tasks':tasks,
-              'plan':plan,
-              'blocking':blocking,
-              'yesterday':yesterday,
-              'result':result,
-              'blocked':blocked,
-              'comment':comment,
-              'feeling':feeling,
-              'timestamp':timestamp,
-              'day':day
+            'projects':projects,
+            'tasks':tasks,
+            'plan':plan,
+            'blocking':blocking,
+            'yesterday':yesterday,
+            'result':result,
+            'blocked':blocked,
+            'comment':comment,
+            'feeling':feeling,
+            'timestamp':timestamp,
+            'day':day
           }
         ))
         try:
@@ -198,6 +231,7 @@ class TRAD:
             self.__last_failed = True
         return False
     def __init__(self):
+        self.__logined = False
         self.__last_failed = False
         self.__last_response = '';
         self.__hash = "null"
